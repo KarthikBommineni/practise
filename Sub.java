a66697e01b7196d38fe4207305938a6ac1e1f7da
karthikbommineni <karthik.bommineni95@gmail.com>
2018-01-08 17:05:43 +0530

    commit

---
+++ Sub.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sub {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner s = new Scanner(System.in);
+			System.out.println("enter the name");
+			String name= s.nextLine();
+			System.out.println("enter the branch");
+			String branch = s.nextLine();
+			System.out.println("enter age");
+			int age=s.nextInt();
+	}
+
+}
